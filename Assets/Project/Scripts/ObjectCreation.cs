7f9d0e351ad2a54b09f9c0eb709b6c72caf47b8b
gerarddoblas <gerard.doblas@enti.cat>
2024-11-08 16:18:35 +0100

    Items Creation Scriptable

--- Assets/Project/Scripts/ObjectCreation.cs
+++ Assets/Project/Scripts/ObjectCreation.cs
@@ -3,19 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[CreateAssetMenu(fileName = "Items",menuName = "Categoria 1/Categoria 2/Categoria 3")]
+[CreateAssetMenu(fileName = "Items",menuName = "ObjectCreatorManager")]
 public class ObjectCreation : ScriptableObject
 {
     List<GameObject> smallObjects = new List<GameObject>();
     List<GameObject> mediumObjects = new List<GameObject>();
     List<GameObject> bigObjects = new List<GameObject>();
-    GameObject GetRandomObject(List<GameObject> objectList) 
-    { 
-        if(objectList.Count == 0)
-        {
-            Debug.LogWarning("La lista is empty.");
-            return null;
-        }
-        return objectList[Random.Range(0,objectList.Count-1)];
-    }
+    GameObject GetRandomSmallObject() { return smallObjects[Random.Range(0, smallObjects.Count - 1)]; }
+    GameObject GetRandomMediumObject() { return mediumObjects[Random.Range(0, mediumObjects.Count - 1)]; }
+    GameObject GetRandomBigObject() { return bigObjects[Random.Range(0, bigObjects.Count - 1)]; }
 }
