baede939357bbbc6797258ad1447673edfdb70ed
eJwtzMFqhDAQgOF3mXMOjdqouZWFUsr2UCg99BLGcbRZo0kTbSrLvnth8frD918BiTilDz/xAhoIH2vkRsmmVEpJqnhou+KhxlIyVVRXDRVKMoKAEP2v7Tke9HqczHqsxm9vTvnlK9d0Ppd9K1/X55999n0+je3wVJSf8vLWvl9AwJ2YdQ8MGjrGyBEEJPL3gP1sFx22zlkyE+8iMvbax1FEDl5siaPmGa0T2cdpcD6DAP4LNnIyuIJeNudut39NKU/3 <gerard.doblas@enti.cat>
2024-11-08 10:04:14 +0100

    Code improvements

--- Assets/Project/Scripts/ObjectCreation.cs
+++ Assets/Project/Scripts/ObjectCreation.cs
@@ -2,8 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu(fileName = "Items",menuName = "Categoria 1/Categoria 2/Categoria 3")]
 public class ObjectCreation : ScriptableObject
 {
-    List<GameObject> objects = new List<GameObject>();
-    GameObject GetRandomObject() { return objects[Random.Range(0,objects.Count-1)]; }
+    List<GameObject> smallObjects = new List<GameObject>();
+    List<GameObject> mediumObjects = new List<GameObject>();
+    List<GameObject> bigObjects = new List<GameObject>();
+    GameObject GetRandomObject(List<GameObject> objectList) 
+    { 
+        if(objectList.Count == 0)
+        {
+            Debug.LogWarning("La lista is empty.");
+            return null;
+        }
+        return objectList[Random.Range(0,objectList.Count-1)];
+    }
 }
