8b1664668243ed7f897dacccfb1ef666f9a1d6d1
eugenivasiliev <eugeni.vasiliev@enti.cat>
2024-11-10 14:07:06 +0100

    Reworked tiles to automatically setup themselves on screen and made them slightly more flexible for different implementations

--- Assets/Project/Scripts/TilemapScript.cs
+++ Assets/Project/Scripts/TilemapScript.cs
@@ -9,17 +9,7 @@ public class TilemapScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        tm = GetComponent<Tilemap>();
-        for (int i = -2; i < 2; i++)
-        {
-            ExplodingTile et = (ExplodingTile)tm.GetTile(new Vector3Int(i, -1, 0));
-            if (et != null && et.started)
-            {
-                et.started = false;
-                Debug.Log("refreshing at " + new Vector3Int(i, -1, 0));
-                tm.RefreshTile(new Vector3Int(i, -1, 0));
-            }
-        }
+
     }
 
     // Update is called once per frame
@@ -30,7 +20,7 @@ public class TilemapScript : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        tm.RefreshTile(new Vector3Int((int)Mathf.Floor(collision.transform.position.x - 0.6f), (int)Mathf.Floor(collision.transform.position.y - 0.6f), 0));
-        Debug.Log(new Vector3Int((int)Mathf.Floor(collision.transform.position.x - 0.6f), (int)Mathf.Floor(collision.transform.position.y - 0.6f), 0));
+        tm.RefreshTile(new Vector3Int((int)Mathf.Ceil(collision.contacts[0].point.x) - 1, (int)(collision.contacts[0].point.y - 1f), 0));
+        //TODO: Check edge cases
     }
 }
