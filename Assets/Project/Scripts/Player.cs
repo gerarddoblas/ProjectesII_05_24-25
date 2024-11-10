0e29094ae0c69bb6eaa55677cc77200207dacd6e
DESKTOP-10JPS7V\sergi <sergigonzalezberlanga@gmail.com>
2024-11-10 12:30:50 +0100

    Player movement remake

--- Assets/Project/Scripts/Player.cs
+++ Assets/Project/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float score;
     public float Score { set { score = value; } get { return score; } }
-    public float speed = 1f, jumpForce = 10f;
+    public float acceleration ,maxSpeed, jumpForce;
     public Vector2 playerSpeed = Vector2.zero;
     public UnityEvent<float> onAlterMana;
     public UnityEvent<float> onAlterScore;
@@ -25,11 +25,15 @@ public class Player : MonoBehaviour
         input.actions.FindAction("Move").performed += Move;
         input.actions.FindAction("Move").canceled += MoveCancelled;
         input.actions.FindAction("Jump").started += Jump;
-
     }
     private void FixedUpdate()
     {
-        rigidbody2D.velocity = new Vector2(playerSpeed.x * speed * Time.deltaTime, rigidbody2D.velocity.y);
+        //rigidbody2D.velocity = new Vector2(playerSpeed.x * speed * Time.deltaTime, rigidbody2D.velocity.y);
+        rigidbody2D.AddForce(new Vector2(acceleration*playerSpeed.x*Time.deltaTime,0), ForceMode2D.Force);
+        if (rigidbody2D.velocity.x > maxSpeed)
+            rigidbody2D.velocity = new Vector2(maxSpeed, rigidbody2D.velocity.y);
+        else if (rigidbody2D.velocity.x < -maxSpeed)
+            rigidbody2D.velocity = new Vector2(-maxSpeed, rigidbody2D.velocity.y);
     }
     private void Update()
     {
