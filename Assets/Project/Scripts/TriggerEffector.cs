ac0af602693d9c67e315078a9d1ebf5aee1f42c1
Eugeni Vasiliev Vovk <eugeni.vasiliev@enti.cat>
2024-11-25 16:46:35 +0100

    test

--- Assets/Project/Scripts/TriggerEffector.cs
+++ Assets/Project/Scripts/TriggerEffector.cs
@@ -8,4 +8,5 @@ public class TriggerEffector : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision){ onEntry.Invoke();}
     private void OnTriggerStay2D(Collider2D collision){onStay.Invoke();}
     private void OnTriggerExit2D(Collider2D collision){onExit.Invoke();}
+
 }
