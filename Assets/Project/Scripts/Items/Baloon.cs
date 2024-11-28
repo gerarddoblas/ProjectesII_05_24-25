8ed6aed0d9f21b043debf6bdc48a77cd1accc280
DESKTOP-10JPS7V\sergi <sergigonzalezberlanga@gmail.com>
2024-11-28 22:35:23 +0100

    Items updated

--- Assets/Project/Scripts/Items/Baloon.cs
+++ Assets/Project/Scripts/Items/Baloon.cs
@@ -8,8 +8,8 @@ public class Baloon : Item
     
     override public void Effect(GameObject target)
     {
-        target.GetComponent<Player>().jumpForce += .5f;
-        if (target.GetComponent<Player>().jumpForce > 10)
-            target.GetComponent<Player>().jumpForce = 10;
+        target.GetComponent<Player>().jumpForce += 1.5f;
+        if (target.GetComponent<Player>().jumpForce > 35)
+            target.GetComponent<Player>().jumpForce = 35;
     }
 }
--- Assets/Project/Scripts/Items/Egg.cs
+++ Assets/Project/Scripts/Items/Egg.cs
@@ -18,9 +18,9 @@ public class Egg : Item {
     }
     override public void Effect(GameObject target)
     {
-        /*if(target.TryGetComponent<Tilemap>(out Tilemap tm))
-                tm.RefreshTile(new Vector3Int((int)Mathf.Floor(target.transform.position.x - 0.6f), (int)Mathf.Floor(target.transform.position.y - 0.6f), 0));
-        */
+        if (target.TryGetComponent<Tilemap>(out Tilemap tm))
+            tm.GetTile(new Vector3Int((int)transform.position.x,(int)transform.position.y));
+
         if (target.TryGetComponent<Player>(out Player p))
         {
             p.GetComponent<Rigidbody2D>().velocity = new Vector2(this.GetComponent<Rigidbody2D>().velocity.x, target.GetComponent<Rigidbody2D>().velocity.y);
