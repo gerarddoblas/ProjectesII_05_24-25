3a80b4047bdfa02b54c7e224fbb2774d9f6d2637
DESKTOP-10JPS7V\sergi <sergigonzalezberlanga@gmail.com>
2024-12-22 12:29:53 +0100

    New title screen animation + music

--- Assets/Project/Scripts/TitleScreen.cs
+++ Assets/Project/Scripts/TitleScreen.cs
@@ -6,9 +6,9 @@ public class TitleScreen : MonoBehaviour
 {
     private void Awake()
     {
-        LeanTween.move(Camera.main.gameObject, Vector2.zero, 2).setOnComplete(delegate ()
+        LeanTween.move(Camera.main.gameObject, Vector2.zero, 7.5f).setOnComplete(delegate ()
         {
                 PlayersManager.Instance.SetJoining(true);
-        }).setEaseInOutElastic();  
+        }).setEaseInOutBounce();  
     }
 }
