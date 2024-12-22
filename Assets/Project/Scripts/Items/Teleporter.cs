f9fa8e3efaa016fcbc36f75e7cb3ecb3c28d1e90
DESKTOP-10JPS7V\sergi <sergigonzalezberlanga@gmail.com>
2024-12-22 12:57:04 +0100

    EnderPearl teleporter

---
+++ Assets/Project/Scripts/Items/Teleporter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Teleporter : Item
+{
+    override public IEnumerator Effect(GameObject target)
+    {
+        
+        if (target.TryGetComponent<Player>(out Player p))
+        {
+            Vector3 creatorPos = creator.transform.position; 
+            creator.transform.position = target.transform.position;
+            target.transform.position = creatorPos;
+        }
+        else
+            creator.transform.position = this.transform.position;
+        Destroy(this.gameObject);
+        yield return null;
+    }
+}
