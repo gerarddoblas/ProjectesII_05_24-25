3b4a57c4cec04ae13c63b525f7dc47cb3e185fd5
DESKTOP-10JPS7V\sergi <sergigonzalezberlanga@gmail.com>
2025-01-11 18:01:57 +0100

    New tile palette

--- Assets/Project/Scripts/TitleScreen.cs
+++ Assets/Project/Scripts/TitleScreen.cs
@@ -1,15 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TitleScreen : MonoBehaviour
 {
+    Controls controls;
     private void Awake()
     {
         LeanTween.move(Camera.main.gameObject, Vector2.zero, 7.5f).setOnComplete(delegate ()
         {
                 PlayersManager.Instance.SetJoining(true);
-        }).setEaseInOutBounce();  
+        }).setEaseInOutBounce();
         
     }
 }
