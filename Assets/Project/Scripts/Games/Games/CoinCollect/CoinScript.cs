0a7a1ccc8a8674ec159525f478217065f2de9e89
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-02-28 10:35:10 +0100

    Fixed coins for gameplay

--- Assets/Project/Scripts/Games/Games/CoinCollect/CoinScript.cs
+++ Assets/Project/Scripts/Games/Games/CoinCollect/CoinScript.cs
@@ -19,9 +19,12 @@ public class CoinScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Player player = collision.GetComponent<Player>();
+        Debug.Log("Coin Collided");
         if (player != null)
         {
+            Debug.Log("Collided with player");
             player.Score++;
+            Destroy(this.gameObject);
         }
     }
 }
