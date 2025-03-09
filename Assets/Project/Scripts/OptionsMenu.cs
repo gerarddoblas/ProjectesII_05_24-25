61308cb2ae25ef043b3c9e7a6f4b663ae0215808
DESKTOP-10JPS7V\sergi <sergigonzalezberlanga@gmail.com>
2025-03-09 15:37:35 +0100

    Options menu

--- Assets/Project/Scripts/OptionsMenu.cs
+++ Assets/Project/Scripts/OptionsMenu.cs
@@ -5,10 +5,25 @@ using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
+    public Toggle fullscreen;
+    public Slider brightSlider;
+    public Slider musicSlider,fxSoundSlider;
     public Button Return;
     void Start()
     {
-        
+        fullscreen.onValueChanged.AddListener(delegate (bool r) { 
+            Screen.fullScreen = r;
+        });
+        brightSlider.onValueChanged.AddListener(delegate (float r) {
+            Screen.brightness = r;
+        });
+        musicSlider.onValueChanged.AddListener(delegate (float r) { 
+            AudioManager.instance.manaSource.volume = r;
+        });
+        fxSoundSlider.onValueChanged.AddListener(delegate (float r) {
+            AudioManager.instance.sfxSource.volume = r;
+            AudioManager.instance.manaSource.volume = r;
+        });
     }
 
     // Update is called once per frame
